b959f715f5937bae468c7e316a8d911d445e30b8
mbreno-op <84518115+mbreno-op@users.noreply.github.com>
2023-06-20 16:58:17 -0300

    Profile info App

--- App.js
+++ App.js
@@ -1,42 +1,54 @@
-import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 
-import Capa from './assets/capa.png';
+import Capa from './assets/capa.jpg';
+import PFP from './assets/pfp.jpg';
 
 export default function App() {
   return (
     <View style={ estilos.container }>
-      <Text style={ estilos.titulo }>Guerra do Amanhã</Text>
-      <Text style={ estilos.descricao }>Gêneros: Ação e ficção</Text>
-      
-      <Image 
-        source={ Capa } 
-        style={ estilos.img }
-      />
+        <Image style={ estilos.capa } source={ Capa }/>
 
+      <View style={ estilos.container }>
+        <Image style={ estilos.img } source={ PFP }/>
+        <Text style={ estilos.titulo }>Michael Breno</Text>
+        <Text style={ estilos.descricao }>Programador Front-end</Text>
+      </View>
     </View>
   );
 }
 
 const estilos = StyleSheet.create({
-
+  
   container: {
+    height: '80%',
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1E1E2E',
     alignItems: 'center',
     justifyContent: 'center',
   },
-
+  
   titulo: {
     fontSize: 22,
+    color: '#CDD6F4',
   },
+
   descricao: {
-    marginBottom: 20
+    marginTop: 20,
+    color: '#CDD6F4',
   },
-  
+
   img: {
-    width: 300,
-    height: 300,
+    width: 200,
+    height: 200,
+    marginTop: 20,
+    marginBottom: 30,
+    borderRadius: 100,
+    borderWidth: 3,
+    borderColor: '#6C7086',
   },
-  
+
+  capa: {
+    flex: 1,
+    width: '100%',
+  },
 });
